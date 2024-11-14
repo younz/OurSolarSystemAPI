f87fe7a870278552e239299ec269e3659c994075
younz <younas1996@live.dk>
2024-11-14 10:31:49 +0100

    Add new Planet model and update project dependencies
    Updated OurSolarSystemAPI.csproj:
    - Removed Swashbuckle.AspNetCore 6.4.0
    - Added MySql.Data 9.1.0
    - Added Newtonsoft.Json 13.0.3
    - Added Newtonsoft.Json.Bson 1.0.3
    - Added Swashbuckle.AspNetCore 7.0.0
    - Added System.Text.Json 9.0.0
    
    Created OurSolarSystemAPI.sln:
    - Solution file format version 12.00
    - Visual Studio version 17
    - Included OurSolarSystemAPI project with GUID
    - Configurations for Debug|Any CPU and Release|Any CPU
    - Solution properties and extensibility globals
    
    Added Planet.cs to OurSolarSystemAPI.Models:
    - Defined Planet class with various properties
    - Included BarrycenterId as a foreign key

---
+++ Models/Planet.cs
@@ -0,0 +1,55 @@
+﻿namespace OurSolarSystemAPI.Models
+{
+    public class Planet
+    {
+        public string Name { get; set; }
+        public string volumeMeanRadius { get; set; }
+        public string density { get; set; }
+        public string mass { get; set; }
+        public string flattening { get; set; }
+        public string Volume { get; set; }
+
+        public string equatorial_radius { get; set; }
+
+        public string sidereal_rotation_period { get; set; }
+
+        public string sidereal_rotation_rate { get; set; }
+
+        public string mean_solar_day { get; set; }
+
+        public string polar_gravity { get; set; }
+
+        public string equatorial_gravity { get; set; }
+
+        public string geometric_albedo { get; set; }
+
+        public string mass_ratio_to_sun { get; set; }
+
+        public string mean_temperature { get; set; }
+
+        public string atmospheric_pressure { get; set; }
+
+        public string obliquity_to_orbit { get; set; }
+
+        public string max_angular_diameter { get; set; }
+
+        public string mean_side_real_orbital_period { get; set; }
+
+        public string orbital_speed { get; set; }
+
+        public string hills_sphere_radius { get; set; }
+
+        public string escape_speed { get; set; }
+
+        public string solar_constant { get; set; }
+
+        public string maximum_planetary_ir { get; set; }
+
+        public string minimum_planetary_ir { get; set; }
+
+        public string gravitational_parameter { get; set; }
+        // foreignId 
+        public int BarrycenterId { get; set; }
+
+    }
+}

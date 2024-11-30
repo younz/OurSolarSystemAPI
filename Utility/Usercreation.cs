50b25be7e5295fad63d4bc5013a6aade54d0cc28
younas kronborg abachri <younas1996@live.dk>
2024-11-30 16:57:37 +0100

    Add async user retrieval and user creation methods
    Updated using directives to include necessary namespaces.
    Added comment to describe the purpose of the Usercreation class.
    Removed the Create method.
    Added GetallUsersAsync method to retrieve all users asynchronously.
    Added placeholder GetUser method to retrieve a user by ID.
    Added CreateUser method to create and add users or admins to the database.
    Updated VerifyPassword method to be static and throw exceptions on failure.

--- Utility/Usercreation.cs
+++ Utility/Usercreation.cs
@@ -1,23 +1,62 @@
 ﻿using OurSolarSystemAPI.Models;
-
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 namespace OurSolarSystemAPI.Utility
 {
     public class Usercreation
     {
+        // this class is used to create users and admins and add them to the database
 
-        public void Create(User user)
-        { // this part checks if the user is an admin and if so, it creates an admin object instead of a user object to add to the database
+        public async Task<List<User>> GetallUsersAsync()
+        {
+            List<User> users = new List<User>();
+            string connectionString = "your_connection_string_here"; // Replace with your actual connection string
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                string query = "SELECT Id, Username, Password, Roles FROM Users"; // Adjust the query as needed
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            User user = new User
+                            {
+                                Id = reader.GetInt32(0),
+                                Username = reader.GetString(1),
+                                Password = reader.GetString(2),
+                                Roles = (roles)Enum.Parse(typeof(roles), reader.GetString(3))
+                            };
+                            users.Add(user);
+                        }
+                    }
+                }
+            }
+
+            return users;
+        }
+
+        public User GetUser(int id)
+        {
+            // Implementation here
+        }
+
+        public void CreateUser(User user)
+        {
+            // this part checks if the user is an admin and if so, it creates an admin object instead of a user object to add to the database
             if (user.Roles is roles.Admin)
             {
                 user = new Admin { Id = user.Id, Username = user.Username, Password = user.Password, Roles = roles.Admin };
             }
-
-
         }
 
-        public bool VerifyPassword(string inputPassword, User user)
+        public static bool VerifyPassword(string inputPassword, User user)
         {
-            string hashedInputPassword = User.HashPassword(inputPassword);
+            string hashedInputPassword = user.HashPassword(inputPassword);
             if (user.Password == hashedInputPassword)
             {
                 return true;
@@ -26,4 +65,3 @@ namespace OurSolarSystemAPI.Utility
             throw new Exception("Password is incorrect");
         }
     }
-}
